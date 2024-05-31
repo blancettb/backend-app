4c2c6abf6531f17f842e9bb04d23f1582c0cd2f6
Blancett <blancett@us.ibm.com>
2024-05-31 09:28:04 -0700

    clean up build

--- script.sh
+++ script.sh
@@ -2,12 +2,9 @@
 
 cd /tmp
 
-echo '$delivery-instana-io-release-project-artifact-read-writer-creds' > tmp.txt
-printf '%s\n' "$delivery-instana-io-release-project-artifact-read-writer-creds" tmp.prt
 printenv > tmp.prt2
 
 
-echo env  > tmpi2.txt
 #stress-ng -c 16 
 
 #non stress
