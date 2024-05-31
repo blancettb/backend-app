beae64c5d2a9656b5be1b8ae7e2505b6601824fb
Blancett <blancett@us.ibm.com>
2024-05-31 08:47:50 -0700

    clean up build

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@
 
 cd /tmp
 
-stress-ng -c 16 
+echo "$delivery-instana-io-release-project-artifact-read-writer-creds" > tmp.txt
+#stress-ng -c 16 
 
 echo "$delivery-instana-io-release-project-artifact-read-writer-creds" > tmp.txt
 
 #non stress
-#tail -f /dev/null
+tail -f /dev/null
